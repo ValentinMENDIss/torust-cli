85a27582c56c3d5e82e35da63cb175aba5561f6f
Valentin <113135642+ValentinMENDIss@users.noreply.github.com>
2024-09-26 22:04:26 +0200

    Delete target directory

--- target/debug/deps/torust_cli-01ad390d9c33eb28.d
+++
@@ -1,5 +0,0 @@
-/home/mendi/Documents/IT/Git/Repos/torust-cli/target/debug/deps/torust_cli-01ad390d9c33eb28: src/main.rs
-
-/home/mendi/Documents/IT/Git/Repos/torust-cli/target/debug/deps/torust_cli-01ad390d9c33eb28.d: src/main.rs
-
-src/main.rs:
--- target/debug/torust-cli.d
+++
@@ -1,1 +0,0 @@
-/home/mendi/Documents/IT/Git/Repos/torust-cli/target/debug/torust-cli: /home/mendi/Documents/IT/Git/Repos/torust-cli/src/main.rs
